7af4da61116f79cbfed64b4e079be5e53cdbbcff
sherlock <mhxy131466@163.com>
2018-12-18 14:39:16 +0800

    Add handle.c

--- PyGitScript/PyGitScript/PyGitScript.py
+++ PyGitScript/PyGitScript/PyGitScript.py
@@ -62,7 +62,7 @@ def on_timer():
 	for current_commit_file_path in current_commit_list:
 		#获取文件名称
 		file_name = os.path.split(current_commit_file_path)[-1]
-		file_name_list.append(file_name)
+		file_name_list.append(file_name.encode('utf-8'))
 		#按行读取文件中所有数据（过滤掉换行符）
 		with open(current_commit_file_path,'r') as f:
 			data = f.read()
@@ -83,7 +83,7 @@ def on_timer():
 		comment= random.sample(match_list,1)[0]
 	#按,分割文件的提交信息
 	commit_file_info = ",".join(file_name for file_name in file_name_list)
-	print(gitobj.commit("-a",'-m', 'Add ' + commit_file_info + " " + comment)) #打印提交信息
+	print(gitobj.commit("-a",'-m', 'Add ' + commit_file_info + " " + comment.encode('utf-8'))) #打印提交信息
 	print(gitobj.push("-u","origin","master"))#打印推送信息
 	#如果还有文件可读，且当前模式支持随机时间，则清空当前所有任务并计算随机时间执行
 	if time_val < 0:
