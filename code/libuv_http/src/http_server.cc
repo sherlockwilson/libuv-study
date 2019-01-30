adf153097336a580a1348748b0746183497e4775
sherlock <471113721@qq.com>
2019-01-30 23:11:58 +0800

    mod get poty

--- code/libuv_http/src/http_server.cc
+++ code/libuv_http/src/http_server.cc
@@ -15,11 +15,15 @@ bool HttpServer::Init(
     int port) {
     struct sockaddr_in addr;
     uv_ip4_addr(ip, port, &addr);
-    ip_ = ip;
-    port_ = ntohs(addr.sin_port);
     url_ = static_path;
     uv_tcp_init(uv_default_loop(), &server_);
     uv_tcp_bind(&server_, (const struct sockaddr*) &addr, 0);
+    char addr_storage[sizeof(struct sockaddr_in)] = {0};
+    int addrlen(sizeof(addr_storage));
+    uv_tcp_getsockname(&server_,(struct sockaddr*) &addr_storage, &addrlen);
+    ip_ = ip;
+    port_ = ntohs(((struct sockaddr_in*) &addr_storage)->sin_port);
+    printf("ip:%s,port:%d\n",ip_.c_str(),port_);
     uv_listen((uv_stream_t*)&server_, 8, HttpServerCallBack::OnConnection);
 
     return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
